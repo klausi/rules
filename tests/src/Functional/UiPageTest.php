5eb5b2a1622d60769b9bae7ffc18839f79d1d980
Klaus Purer <klaus.purer@gmail.com>
2015-11-30 18:13:43 +0100

    Added browser test case for overview page

---
+++ tests/src/Functional/UiPageTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Contains
+ */
+
+namespace Drupal\Tests\rules\Functional;
+
+use Drupal\simpletest\BrowserTestBase;
+
+/**
+ * Tests that the Rules UI pages a reachable.
+ *
+ * @group rules_ui
+ */
+class UiPageTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['rules'];
+
+  /**
+   * Tests that the reaction rule listing page works.
+   */
+  public function testReactionRulePage() {
+    $account = $this->drupalCreateUser(['administer rules']);
+    $this->drupalLogin($account);
+
+    // Visit a Drupal page that requires login.
+    $this->drupalGet('admin/config/workflow/rules');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Test that there is an empty reaction rule listing.
+    $this->assertSession()->pageTextContains('There is no Reaction Rule yet.');
+  }
+
+}
