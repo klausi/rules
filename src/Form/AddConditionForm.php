9fec555dadeb9dce135125bc472ef123a02227ef
Klaus Purer <klaus.purer@gmail.com>
2015-12-19 18:47:06 +0100

    Implemented saving of direct input mode vs data selector

--- src/Form/AddConditionForm.php
+++ src/Form/AddConditionForm.php
@@ -45,7 +45,7 @@ class AddConditionForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, ReactionRuleConfig $reaction_config = NULL) {
     $form_state->set('reaction_config', $reaction_config);
-    $condition_name = $form_state->getValue('condition');
+    $condition_name = $form_state->get('condition');
 
     // Step 1 of the multistep form.
     if (!$condition_name) {
@@ -109,14 +109,24 @@ class AddConditionForm extends FormBase {
       '#type' => 'fieldset',
       '#title' => $context_definition->getLabel(),
     ];
+    $form['context'][$context_name]['description'] = [
+      '#markup' => $context_definition->getDescription(),
+    ];
+
+    $mode = $form_state->get('context_' . $context_name, 'input');
+    $title = $mode == 'selector' ? $this->t('Data selector') : $this->t('Value');
+    // @todo get a description for possible values that can be filled in.
+    $description = $mode == 'selector'
+      ? $this->t("The data selector helps you drill down into the data available to Rules. <em>To make entity fields appear in the data selector, you may have to use the condition 'entity has field' (or 'content is of type').</em> More useful tips about data selection is available in <a href=':url'>the online documentation</a>.", [
+        ':url' => 'https://www.drupal.org/node/1300042',
+      ]) : '';
     $form['context'][$context_name]['setting'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Value'),
-      '#description' => $context_definition->getDescription(),
+      '#title' => $title,
+      '#description' => $description,
       '#required' => $context_definition->isRequired(),
     ];
 
-    $mode = 'input';
     $value = $mode == 'selector' ? $this->t('Switch to the direct input mode') : $this->t('Switch to data selection');
     $form['context'][$context_name]['switch_button'] = [
       '#type' => 'submit',
@@ -131,7 +141,15 @@ class AddConditionForm extends FormBase {
     return $form;
   }
 
+  /**
+   * Submit callback: switch a context to data selecor or direct input mode.
+   */
   public function switchContextMode(array &$form, FormStateInterface $form_state) {
+    $element_name = $form_state->getTriggeringElement()['#name'];
+    $mode = $form_state->get($element_name);
+    $switched_mode = $mode == 'selector' ? 'input' : 'selector';
+    $form_state->set($element_name, $switched_mode);
+
     $form_state->setRebuild();
   }
 
@@ -152,7 +170,12 @@ class AddConditionForm extends FormBase {
 
       $context_config = ContextConfig::create();
       foreach ($form_state->getValue('context') as $context_name => $value) {
-        $context_config->setValue($context_name, $value['setting']);
+        if ($form_state->get("context_$context_name") == 'selector') {
+          $context_config->map($context_name, $value['setting']);
+        }
+        else {
+          $context_config->setValue($context_name, $value['setting']);
+        }
       }
 
       $expression->addCondition($form_state->getValue('condition'), $context_config);
@@ -168,6 +191,7 @@ class AddConditionForm extends FormBase {
       ]);
     }
     else {
+      $form_state->set('condition', $form_state->getValue('condition'));
       $form_state->setRebuild();
     }
   }
